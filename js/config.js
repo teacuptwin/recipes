5d6484b3d438375dd3827cf838b926b535ff091a
teacuptwin <teacuptwin@gmail.com>
2026-05-07 16:20:37 +0100

    Update SHEET_ID in config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  SHEET_ID: "2PACX-1vTPB7tNbpUzCnZuG3MS_CukiIDEDP7x27GniFWNw4ID6h9GM_p2LDlgIjqLT2044FhlC6Ora3BCB-gp",    // from step 1
+  SHEET_ID: "1jF_b80AAuFg0zGeja1y5RWoJDZm1OX2pb05S_HWw1uw",    // from step 1
   SHEET_TAB: "Sheet1",                // your sheet tab name
 };
