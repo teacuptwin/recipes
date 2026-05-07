0b66126f731c5c744f12e9425e8ebc5e36405de0
teacuptwin <teacuptwin@gmail.com>
2026-05-07 15:04:16 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,4 @@
 const CONFIG = {
-  PASSWORD: "spicegirls",   // what friends type to enter
   SHEET_ID: "2PACX-1vTPB7tNbpUzCnZuG3MS_CukiIDEDP7x27GniFWNw4ID6h9GM_p2LDlgIjqLT2044FhlC6Ora3BCB-gp",    // from step 1
   SHEET_TAB: "Sheet1",                // your sheet tab name
 };
