7c2b5e8d6f8e2b226e40e6f761b46dd11d8bef8e
teacuptwin <teacuptwin@gmail.com>
2026-05-07 18:25:43 +0100

    Fix config to use Sheet1 tab name

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
   SHEET_ID: "1jF_b80AAuFg0zGeja1y5RWoJDZm1OX2pb05S_HWw1uw",    // from step 1
-  SHEET_TAB: "Main",                // your sheet tab name
+  SHEET_TAB: "Sheet1",                // your sheet tab name
 };
